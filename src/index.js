9ae0f4f0557a1f71f3ab484141ef4d2d3acd7e3b
aksy007 <akshaykolhe04@gmail.com>
2024-02-11 01:33:25 +0530

    setup express app and add utils functions to handle api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
-
-connectDB();
+const PORT = process.env.PORT || 3000;
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Express Error: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running at ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log('MongoDB connection failed !!', error);
+    });
